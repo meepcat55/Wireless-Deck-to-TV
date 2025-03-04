ade88e0f4ae637f3fa42ad22ff0151b808909de7
MeepCat55 <89799752+meepcat55@users.noreply.github.com>
2025-03-04 09:07:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ while choiceloop == 0:
     else:
         print("Invalid choice")
 os.system("flatpak --user install flathub org.mozilla.firefox -y")
+# Im working on porting this to my own ftp server
 print("Download the file opened in google drive in firefox to the downloads folder close firefox when finished")
 os.system("flatpak run org.mozilla.firefox https://drive.google.com/file/d/1B_eYMYBwzQQioxQ9drH0GuSux501-9Qs/view")
 input("Press A once download completed")
